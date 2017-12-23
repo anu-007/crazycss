d2920fc63f89d40f270a7c40f2d7c2e0543ae2cd
anu-007 <anubhutiv1@gmail.com>
2017-12-24 05:10:45 +0530

    initial skeleton for the project

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+gulp.task('sass', () => {
+  gulp.src('src/style.scss')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(cleanCSS({format: 'beautify'}))
+  .pipe(rename('crazy.css'))
+  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch('./src/*.scss',['styles']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('*.css').on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', () => {
+  gulp.src('dist/crazy.css')
+    .pipe(cleanCSS())
+    .pipe(rename('crazy.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('collect', () => {
+  gulp.src('src/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({format: 'beautify'}))
+    .pipe(gulp.dest('dist/components'));
+});
+
+gulp.task('default', ['sass', 'serve']);
+gulp.task('build', ['collect', 'scss', 'minify-css']);
